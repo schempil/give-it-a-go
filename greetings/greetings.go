dcd288e07f78cb1469344de3fbb4026867c11cb4
Philipp Schemel <philippschemel@live.com>
2020-10-06 18:24:54 +0200

    Further step on in official getting started documentation

---
+++ greetings/greetings.go
@@ -0,0 +1,14 @@
+package greetings
+
+import "fmt"
+import "errors"
+
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
+}
---
+++ hello/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, error := greetings.Hello("")
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	fmt.Println(message)
+}
